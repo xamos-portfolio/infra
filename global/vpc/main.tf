c0ce44d8b0ee6e9f852c2e01ec136ea5d39fb7c6
IkenoXamos <moberlies1337@gmail.com>
2026-04-12 06:01:22 -0700

    Remove auto-created default firewall rules

--- global/vpc/main.tf
+++ global/vpc/main.tf
@@ -1,10 +1,11 @@
 # Create the base network
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network
 resource "google_compute_network" "main" {
-  name                    = "main"
-  routing_mode            = "REGIONAL"
-  auto_create_subnetworks = false # Do not create default subnets
-  mtu                     = 1460
+  name                            = "main"
+  routing_mode                    = "REGIONAL"
+  auto_create_subnetworks         = false # Do not create default subnets
+  mtu                             = 1460
+  delete_default_routes_on_create = true
 }
 
 # Create our subnet manually
